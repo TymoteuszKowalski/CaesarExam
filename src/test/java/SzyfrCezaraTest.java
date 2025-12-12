0a8ab8d95a7e2e866f510bcead94603b1df22b28
Parufa <tymek@gft.pl>
2025-12-12 11:55:27 +0100

    Dodanie testu 1

--- src/test/java/SzyfrCezaraTest.java
+++ src/test/java/SzyfrCezaraTest.java
@@ -1,2 +1,11 @@
+import org.example.main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SzyfrCezaraTest {
+    @Test
+    public void testPodstawowyKluczDodatni() {
+        Assertions.assertEquals("def", main.szyfrujTekst("abc", 3));
+    }
+
 }
